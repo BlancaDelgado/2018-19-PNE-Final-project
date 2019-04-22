97823539e6c21cc35d55267df50157bce28bc93c
BlancaDelgado <blancadelgadobonet@gmail.com>
2019-04-22 17:51:31 +0200

    Working: basic project
    - Species DONE

--- server.py
+++ server.py
@@ -4,7 +4,10 @@ import http.server
 import http.client
 import socketserver
 import termcolor
+
 import json
+import requests
+import sys
 
 
 # Protocol: HTTP
@@ -20,31 +23,32 @@ def connect(ENDPOINT):
     :return: json file downloaded from GITHUB.
     """
 
-    HOSTNAME = "http://rest.ensembl.org"
-    METHOD = "GET"
-    headers = {'User-Agent': 'http-client', 'Content-Type': 'application/json'}
+    HOSTNAME = "rest.ensembl.org"
+    METHOD = 'GET'
+    headers = {'content-type': 'application/json'}
 
     # CONNECT
-    termcolor.cprint('\nConnecting: ' + HOSTNAME + ENDPOINT, 'green')
-    conn = http.client.HTTPSConnection(HOSTNAME)
-    conn.request(METHOD, ENDPOINT, None, headers)
+    termcolor.cprint('Connecting: http://' + HOSTNAME + ENDPOINT, 'green')
+    conn = http.client.HTTPConnection(HOSTNAME)
+    conn.request(METHOD, ENDPOINT, '?', headers)
     r = conn.getresponse()
 
     # POLISH INFO
     termcolor.cprint('Response received: {num} {OK}'.format(num=r.status, OK=r.reason), 'green')
     txt_json = r.read().decode("utf-8")
-    conn.close()
-
     data = json.loads(txt_json)
+    conn.close()
     return data
 
 
 class MainHandler(http.server.BaseHTTPRequestHandler):
 
+    socketserver.TCPServer.allow_reuse_address = True
+
     def do_GET(self):
 
         # PRINT REQUEST LINE
-        termcolor.cprint(self.requestline, 'green')
+        termcolor.cprint('\n'+self.requestline, 'green')
 
         # READ FILE DEPENDING ON PATH
         # Main page
@@ -52,10 +56,66 @@ class MainHandler(http.server.BaseHTTPRequestHandler):
             f200 = open('main.html', 'r')
             contents = f200.read()
 
+        # GET: list of species
+        elif 'listSpecies' in self.path:
+
+            endpoint = '/info/species'
+            data = connect(endpoint)['species']
+
+            try:  # set limit, find species
+                if '=' in self.path:
+                    path = self.path.split('?')
+                    limit = path[1].split('=')[1]
+                    limit = int(limit)
+                else:
+                    limit = len(data)
+
+                list_species = []
+                n = 0
+                for i in data:
+                    n += 1
+                    if n > limit:
+                        break  # limit reached
+
+                    name = i['display_name']
+                    list_species.append(str(n) + '. ' + name)
+
+                str_species = '<br>'.join(list_species)
+
+                contents = """
+                <html>
+                    <header>
+                        <meta charset="UTF-8">
+                        <title>Species</title>
+                    </header>
+                    
+                    <body>
+                        <h3><u>SPECIES</u>:</h3>
+                        <p>{species}</p>
+                        <br>
+                        <a href="/">[Main page]</a>
+                        <br><br>
+                    </body>
+                </html>
+                
+                """.format(species=str_species)
+
+            except ValueError:  # unless limit is not valid
+                f02 = open('error_ValueError.html', 'r')
+                contents = f02.read()
+
+        # GET: karyotype
+        elif 'karyotype' in self.path:
+            contents = 'KARYOTYPE'
+
+        # GET: chromosome length
+        elif 'chromosomeLength' in self.path:
+            contents = 'CHROMOSOME'
+
         # Error
         else:
-            f0 = open('error_NoPage.html', 'r')
-            contents = f0.read()
+            f01 = open('error_NoPage.html', 'r')
+            contents = f01.read()
 
         # GET RESPONSE MESSAGE
         self.send_response(200)
@@ -70,7 +130,6 @@ class MainHandler(http.server.BaseHTTPRequestHandler):
 PORT = 8000
 
 with socketserver.TCPServer(('', PORT), MainHandler) as httpd:
-    httpd.allow_reuse_address = True
     print('Serving at PORT: {}'.format(PORT))
 
     try:
